1d9be085e23663b01527d02a4ff7d84af48f14e6
hongvi <vivo74513@gmai.com>
2026-04-21 10:52:37 +0700

    fix deploy

--- app/(commerce)/[slug]/page.tsx
+++ app/(commerce)/[slug]/page.tsx
@@ -6,12 +6,12 @@ import { Metadata } from "next";
 import { CardImageProduct } from "./card-image.comp";
 import { ProductAction } from "./product-action.comp";
 
-// type Props = { params: { slug: string } };
-type Props = {
-  params: Promise<{
-    slug: string
-  }>
-}
+type Props = { params: { slug: string } };
+// type Props = {
+//   params: Promise<{
+//     slug: string
+//   }>
+// }
 
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
